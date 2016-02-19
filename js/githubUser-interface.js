079a77ae6ea6256dd83a607fea219d57682a47cc
kcmdouglas <kcmdouglas@gmail.com>
2016-02-19 15:23:52 -0800

    Add function, export scripts, add new function to githubUser jQuery.

--- js/githubUser-interface.js
+++ js/githubUser-interface.js
@@ -1,7 +1,24 @@
+var scripts = require('./../js/scripts.js');
+
 $(document).ready(function(){
   var responseFromGit;
-   $.get('https://api.github.com/users/daneden', function(response) {
-     responseFromGit = response;
-     if(responseFromGit !==)
-   }
+  $("form").submit(function(){
+    var username = $("input#username").val();
+
+    $.get('https://api.github.com/users/' + username, function(response) {
+       responseFromGit = response;
+       if(responseFromGit === 404) {
+         $("span#failMessage").text("<h3>Whoops! That doesn't seem to be an existing Github username.");
+       } else {
+         $(".hidden").show()
+         var following = response.following;
+         var followers = response.followers;
+         var concatFollow = concatFollowInfo(following, followers);
+         $("div#user-info").append(concatFollow);
+
+
+
+       }
+    });
+  });
 });
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+concatFollowInfo = function(followingNumber, followersNumber) {
+  return "Following <span class='badge'>" + followingNumber + "</span> || Followers <span class='badge'>" + followersNumber "</span>";
+}
+
+module.exports = {
+  concatFollowInfo: concatFollowInfo
+};
