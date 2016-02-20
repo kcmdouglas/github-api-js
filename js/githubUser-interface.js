5d52f5f92c6bc58a87d0edd0eef7884e28892ce4
kcmdouglas <kcmdouglas@gmail.com>
2016-02-19 16:54:23 -0800

    Add SASS styling

--- js/githubUser-interface.js
+++ js/githubUser-interface.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
 
     $.get('https://api.github.com/users/' + username, function(response) {
        responseFromGit = response;
-       if(responseFromGit === 404) {
+       if(responseFromGit !== 404) {
          $("span#failMessage").text("<h3>Whoops! That doesn't seem to be an existing Github username.");
        } else {
          $(".hidden").show()
-         var following = response.following;
-         var followers = response.followers;
+         var following = responseFromGit.following;
+         var followers = responseFromGit.followers;
          var concatFollow = concatFollowInfo(following, followers);
          $("div#user-info").append(concatFollow);
 
